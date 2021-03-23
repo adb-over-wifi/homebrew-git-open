d235378e9ec4929d9bffc6012a74d9dd17da561d
Kaustubh Patange <developerkp16@gmail.com>
2021-03-23 17:45:23 +0530

    [Updated] v0.1.1

--- Formula/git-open.rb
+++ Formula/git-open.rb
@@ -1,9 +1,9 @@
 class git_open < Formula
     desc "A command line tool to open git project website of the containing repository." 
     homepage "https://github.com/KaustubhPatange/git-open"
-    url "https://github.com/KaustubhPatange/git-open/releases/download/v0.1.0/git-open-mac.tar.gz"
-    sha256 "2AA29D5027DA7B5818E3FA35C7ED3194FCE6B017545A6DD7A01F73698E8F65D3"
-    version "0.1.0"
+    url "https://github.com/KaustubhPatange/git-open/releases/download/v0.1.1/git-open-mac.tar.gz"
+    sha256 "096D24ED5D8BBEBC8F69DABFF67318E7F81314ED854FA00E137745CA28ED2A9F"
+    version "0.1.1"
   
     def install
       bin.install "git-open"
